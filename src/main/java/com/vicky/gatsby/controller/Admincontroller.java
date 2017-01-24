e9302fa177d8aed1f13c626feace6de166ae1305
K.Saiteja <ksaiteja844@gmail.com>
2017-01-24 17:54:36 +0530

    Applied Restful webservices to Adminpart

--- src/main/java/com/vicky/gatsby/controller/Admincontroller.java
+++ src/main/java/com/vicky/gatsby/controller/Admincontroller.java
@@ -1,12 +1,14 @@
 package com.vicky.gatsby.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vicky.gatsby.dao.AdminDAO;
 import com.vicky.gatsby.dao.PersonDAO;
 import com.vicky.gatsby.model.Persondetails;
 
@@ -14,12 +16,22 @@ import com.vicky.gatsby.model.Persondetails;
 public class Admincontroller {
 
 	@Autowired
-	PersonDAO p;
+	AdminDAO ad;
 
 	@RequestMapping(value="/changerole/{username}/{role}",method=RequestMethod.GET)
-public ResponseEntity<Persondetails>(@PathVariable("username")String username,)
+public ResponseEntity<Persondetails> roleupdate(@PathVariable("username")String username,@PathVariable("role")String role)
 	{
+		Persondetails per=ad.personroleupdate(username, role);
+		System.out.println("Admin roleupdate controller");
+		return new ResponseEntity<Persondetails>(per,HttpStatus.OK);
+	}
+	@RequestMapping(value="/changestatus/{username}/{status}",method=RequestMethod.GET)
+	public ResponseEntity<Persondetails> statusupdate(@PathVariable("username")String username,@PathVariable("status")String status)
+	{
+
+		Persondetails per=ad.personstatusupdate(username, status);
+		System.out.println("Admin statusupdate controller");
+		return new ResponseEntity<Persondetails>(per,HttpStatus.OK);
 		
-		return new ResponseEntity<Persondetails>(body, headers, status);
 	}
 }
--- src/main/java/com/vicky/gatsby/daoimpl/AdminDAOImpl.java
+++ src/main/java/com/vicky/gatsby/daoimpl/AdminDAOImpl.java
@@ -2,13 +2,15 @@ package com.vicky.gatsby.daoimpl;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.vicky.gatsby.dao.AdminDAO;
 import com.vicky.gatsby.dao.PersonDAO;
 import com.vicky.gatsby.model.Persondetails;
-
+@Repository
 public class AdminDAOImpl implements AdminDAO {
-@Autowired
+
 public SessionFactory sessionFactory;
 
 @Autowired
@@ -18,13 +20,14 @@ public AdminDAOImpl(SessionFactory sessionFactory)
 { 
 	this.sessionFactory=sessionFactory;
 }
+@Transactional
 	public Persondetails personroleupdate(String username, String role) {
 		Persondetails per=(Persondetails) sessionFactory.getCurrentSession().get(Persondetails.class, username);
 per.setRole(role);
 		pers.update(per);
 		return per;
 	}
-
+@Transactional
 	public Persondetails personstatusupdate(String username, String status) {
 	Persondetails per=(Persondetails) sessionFactory.getCurrentSession().get(Persondetails.class,username);
 	per.setStatus(status);
